f190f6d51b8edb52148751c1f8443bcd5549158c
DeplorableJed <48975520+DeplorableJed@users.noreply.github.com>
2019-07-19 15:33:25 +0800

    Update PythonLab10Source.py

--- PythonLab10Source.py
+++ PythonLab10Source.py
@@ -2,7 +2,7 @@ def get_user_sys_id(snow_user):
 
     # find the ServiceNow user_id for the specified user
 
-    url = snow_url + '/table/sys_user?sysparm_limit=1&name=' + snow_user
+    url = snow_url + 'table/sys_user?sysparm_limit=1'
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
     response = requests.get(url, auth=(snow_user, snow_pass), headers=headers)
     user_json = response.json()
@@ -14,8 +14,8 @@ def create_incident(description, comment, snow_user, severity):
     # This function will create a new incident with the {description}, {comments}, severity for the {user}
 
     caller_sys_id = get_user_sys_id(snow_user)
-    print caller_sys_id
-    url = snow_urlL + '/table/incident'
+    print (caller_sys_id)
+    url = snow_url + '/table/incident'
     payload = {'short_description': description,
                'comments': (comment + ', \nIncident created using APIs by caller ' + snow_user),
                'caller_id': caller_sys_id,
